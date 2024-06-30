3fda67fc87b272b5bbbac40417b8937f88e81841
KhristinaVorozhbet93 <vorozhbet.khristina@yandex.ru>
2024-06-30 14:52:01 +0400

    Зарегистрирована зависимость, удалены страницы

--- sources/PetSocialNetwork.App/Program.cs
+++ sources/PetSocialNetwork.App/Program.cs
@@ -1,12 +1,16 @@
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PetSocialNetwork.App;
+using PetSocialNetwork.App.Services.Auth;
+using System.Net.NetworkInformation;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddAuthorizationCore();
 
